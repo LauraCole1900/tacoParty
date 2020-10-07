6a3ea35d79c58527045322a9ba92532d227b9057
John Doe <AmilitelloDU@gmail.com>
2020-10-06 18:24:01 -0600

    added prototype ajax function for api

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,31 @@ $(document).ready(function () {
   
 
 });
+
+function tacoRecipe(){
+$.ajax({
+    method: "GET",
+    url: "https://api.edamam.com/search",
+    data: { app_id:"9febdf63", app_key:"81e8afa3f473280b371839961f1c44a0", q:"taco"},
+    dataType: "json",
+    error: function(jqXHR, textStatus, errorThrown) {
+        console.log("jqXHR"+jqXHR)
+        console.log("-------------------------------")
+        console.log("textStatus"+textStatus)
+        console.log("-------------------------------")
+        console.log("errorThrown"+errorThrown);
+    },
+    success: function(data, textStatus, jqXHR) {
+
+
+        console.dir(data)
+        console.log("-------------------------------")
+        console.log("textStatus: "+textStatus)
+        console.log("-------------------------------")
+        console.log("jqXHR: "+jqXHR);
+  
+      }
+  }
+  );}
+
+  // attempt to access object variables
